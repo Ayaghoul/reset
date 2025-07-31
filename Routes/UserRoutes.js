ac36b683f4abe3d538ab3fd6e8a12d1a6925e7fe
Ayaghoul <eyalghoul@gmail.com>
2025-07-31 19:42:17 +0100

    first commit

--- Routes/UserRoutes.js
+++ Routes/UserRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const userController = require("../controllers/Usercontrollers");
+const userController = require("../Controllers/Usercontrollers");
 
 router.get("/", userController.Getusers);
 
